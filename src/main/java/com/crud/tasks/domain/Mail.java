9078ce705291627e2d38738423ebb9f5a2ac8a23
Filip Witusik <witusikf@gmail.com>
2024-01-14 19:44:34 +0100

    19.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private String toCc;
+    public void setCc(String toCc) {
+        this.toCc = toCc;
+    }
+}
